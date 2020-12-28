999f3f79773e746deb0c9bddcc5b79131d5501e4
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-12-28 16:28:56 +0100

    built components to show dummy places data

---
+++ src/places/components/PlaceItem.js
@@ -0,0 +1,79 @@
+// Imports of Components
+import Card from "../../shared/components/Card";
+
+// Imports for Styling
+import styled from "styled-components";
+
+const PlaceItem = (props) => {
+  return (
+    <StyledPlaceItem>
+      <Card className="place-time__content">
+        <PlaceItemImage>
+          <img src={props.image} alt={props.title} />
+        </PlaceItemImage>
+        <PlaceItemInfo>
+          <h2>{props.title}</h2>
+          <h3>{props.address}</h3>
+          <p>{props.description}</p>
+        </PlaceItemInfo>
+        <PlaceItemActions>
+          <button>VIEW ON MAP</button>
+          <button>EDIT</button>
+          <button>DELETE</button>
+        </PlaceItemActions>
+      </Card>
+    </StyledPlaceItem>
+  );
+};
+
+// Styled Components
+const StyledPlaceItem = styled.li`
+  margin: 1rem 0;
+  .place-item__content {
+    padding: 0;
+  }
+`;
+
+const PlaceItemImage = styled.div`
+  width: 100%;
+  height: 12.5rem;
+  margin-right: 1.5rem;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  @media (min-width: 768px) {
+    height: 20rem;
+  }
+`;
+
+const PlaceItemInfo = styled.div`
+  padding: 1rem;
+  text-align: center;
+  h2,
+  h3,
+  p {
+    margin: 0 0 0.5rem 0;
+  }
+`;
+
+const PlaceItemActions = styled.div`
+  padding: 1rem;
+  text-align: center;
+  border-top: 1px solid #ccc;
+  button,
+  a {
+    margin: 0.5rem;
+  }
+`;
+
+//   .place-item__modal-content {
+//     padding: 0;
+//   }
+
+//   .place-item__modal-actions {
+//     text-align: right;
+//   }
+
+export default PlaceItem;
---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,37 @@
+// Imports of Components
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://de.wikipedia.org/wiki/Empire_State_Building#/media/Datei:Empire_State_Building_from_the_Top_of_the_Rock.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484445,
+      lng: -73.9878531,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://de.wikipedia.org/wiki/Empire_State_Building#/media/Datei:Empire_State_Building_from_the_Top_of_the_Rock.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484445,
+      lng: -73.9878531,
+    },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = () => {
+  return <PlaceList items={DUMMY_PLACES} />;
+};
+
+export default UserPlaces;
