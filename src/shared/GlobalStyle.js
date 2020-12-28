39775c8cbabdb2dcbc7f4f65554a695527ff0323
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-12-28 12:34:07 +0100

    header and nav are shown and work correctly

--- src/shared/GlobalStyle.js
+++ src/shared/GlobalStyle.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap');
 
 *{
     margin: 0;
