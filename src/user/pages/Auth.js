d845a8544a576f7bf215b01274eabf6efef388be
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-09 14:36:14 +0100

    Post request to signup works

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -42,12 +42,6 @@ const Auth = () => {
   );
 
   // Event Handler Functions
-  const authSubmitHandler = (e) => {
-    e.preventDefault();
-    console.log(formState); // Later this gets sent to the backend
-    auth.login();
-  };
-
   const switchModeHandler = () => {
     if (!isLoginMode) {
       setFormData(
@@ -70,6 +64,31 @@ const Auth = () => {
       );
     }
     setIsLoginMode((prevMode) => !prevMode);
+  };
+
+  const authSubmitHandler = async (e) => {
+    e.preventDefault();
+    if (isLoginMode) {
+    } else {
+      try {
+        const response = await fetch("http://localhost:8000/api/users/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+        const responseData = await response.json();
+        console.log(responseData);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    auth.login();
   };
 
   return (
