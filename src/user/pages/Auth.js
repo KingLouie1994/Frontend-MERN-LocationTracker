466b5c212bf2d2daa02419c23aded12ae81174f3
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-13 12:19:12 +0100

    Post request to create new place works

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -75,7 +75,7 @@ const Auth = () => {
     e.preventDefault();
     if (isLoginMode) {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           "http://localhost:8000/api/users/login",
           "POST",
           JSON.stringify({
@@ -86,11 +86,11 @@ const Auth = () => {
             "Content-Type": "application/json",
           }
         );
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {}
     } else {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           "http://localhost:8000/api/users/signup",
           "POST",
           JSON.stringify({
@@ -102,7 +102,7 @@ const Auth = () => {
             "Content-Type": "application/json",
           }
         );
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {}
     }
   };
